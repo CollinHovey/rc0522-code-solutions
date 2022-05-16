0e0c6688d722a837e9a40c5568695bf699d3e54f
Collin <collinhovey@gmail.com>
2022-05-16 18:36:20 +0000

    Completed Javascript Operators and Expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 20;
+var height = 20;
+var area = height * width;
+console.log('valueof area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 200;
+var payment = 400;
+var change = payment - bill;
+console.log('value0f change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 90;
+var midterm = 82;
+var final = 86;
+var grade = (quizzes + midterm + final) / 3;
+console.log('valueof grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Collin';
+var lastName = 'Hovey';
+var fullName = firstName + ' ' + lastName;
+console.log('valueof fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 5;
+var isAcidic = pH < 7;
+console.log('valueof isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 234;
+var isSparta = headCount === 300;
+console.log('valueof isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto = motto + ' is the GOAT';
+console.log('valueof motto:', motto);
+console.log('typeof motto:', typeof motto);
