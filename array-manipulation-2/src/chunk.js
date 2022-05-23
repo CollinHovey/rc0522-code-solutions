5385194330eba411c4fb9dc6b2339c75fa6fd4bf
Collin <collinhovey@gmail.com>
2022-05-23 07:04:33 +0000

    Completed Array Manipulation 2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -2,10 +2,38 @@
 function chunk(array, size) {
   var newArray = [];
   for (let x = 0; x < array.length; x++) {
-    if (newArray[(array.length - 1)].length === size || newArray === []) {
+    if (newArray.length === 0) {
       newArray.push([]);
+      newArray[0].push(array[x]);
+    } else if (newArray[(newArray.length - 1)].length === size) {
+      newArray.push([]);
+      newArray[newArray.length - 1].push(array[x]);
+    } else {
+      newArray[newArray.length - 1].push(array[x]);
     }
-    newArray[array.length - 1].push(array[x]);
   }
   return newArray;
 }
+
+// var chunkquantity = array.length / size;
+// console.log(chunkquantity);
+// if (array.length % size !== 0) {
+//   Math.ceil(chunkquantity);
+// }
+
+// for (let x = 0; x < array.length; x++) {
+//   if (newArray === []) {
+//     var chunkArray = [];
+//     chunkArray.push(array[x]);
+//     console.log('new chunk array', chunkArray);
+//     newArray.push(chunkArray);
+//     console.log('Array After Push', newArray);
+//   } else if (chunkArray.length === 2) {
+//     var newChunkArray = [];
+//     newChunkArray.push(array[x]);
+//     console.log('new chunk array', newChunkArray);
+//     newArray.push(newChunkArray);
+//     console.log('Array After Push', newArray);
+//   }
+// }
+// return newArray;
