11315e4c94d7b37aca93297ee75ab3ecaa1fae15
Collin <collinhovey@gmail.com>
2022-05-23 18:28:25 +0000

    Completed Dom Querying

---
+++ dom-querying/main.js
@@ -0,0 +1,18 @@
+console.log('hello, world');
+var header = document.querySelector('h1');
+console.log('Header', header);
+console.dir(header);
+
+var explain = document.querySelector('#explanation');
+console.log('Explain', explain);
+console.dir(explain);
+
+var hint = document.querySelector('.hint');
+console.log('Hint', hint);
+console.dir(hint);
+
+var paragraph = document.querySelectorAll('p');
+console.log('Paragraphs', paragraph);
+
+var example = document.querySelectorAll('.example-link');
+console.log('Header', example);
