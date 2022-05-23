7dd3be52f1e23bdf40888f9b2ceb4847826ed012
Collin <collinhovey@gmail.com>
2022-05-23 20:21:21 +0000

    Completed Object Manipulation 2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,22 +1,9 @@
 /* exported defaults */
-// function defaults(target, source) {
-//   console.log(target);
-//   for (var x in source) {
-//     console.log('Source', x);
-//     for (var y in target);
-//     if (x === y) {
-//       continue;
-//       console.log('true');
-//     }
-//     target[x] = source[x];
-//   }
-// }
-// console.log(target);
 
-// function defaults(target, source) {
-//   console.log(target);
-//   for (var x in source) {
-//     target[x] = source[x];
-//   }
-//   console.log(target);
-// }
+function defaults(target, source) {
+  for (var x in source) {
+    if (target[x] === undefined) {
+      target[x] = source[x];
+    }
+  }
+}
