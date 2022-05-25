180735ab31b647ee01d3776400d0e46b36e25de1
Collin <collinhovey@gmail.com>
2022-05-25 10:47:39 +0000

    Some Extra Updates

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,6 +1,9 @@
 var $correct = document.querySelector('span.correct');
 var $currentLetter = document.querySelector('span.current-letter');
 var $rest = document.querySelector('span.rest');
+var $div = document.querySelector('div');
+var correctCounter = 0;
+var totalCounter = 0;
 
 window.addEventListener('keypress', whatKey);
 
@@ -10,7 +13,20 @@ function whatKey(e) {
     $currentLetter.textContent = $rest.textContent[0];
     $rest.textContent = $rest.textContent.slice(1, $rest.textContent.length);
     $currentLetter.className = 'current-letter';
+    correctCounter += 1;
+    totalCounter += 1;
   } else {
     $currentLetter.className = 'current-letter-wrong';
+    totalCounter += 1;
+  }
+  if ($currentLetter.textContent === '') {
+    var finalStat = (correctCounter / totalCounter) * 100;
+    var $stats = document.createElement('h2');
+    $stats.textContent = 'You scored ' + finalStat + '%';
+    $div.append($stats);
+    var $playAgain = document.createElement('button');
+    $playAgain.textContent = 'PLAY AGAIN?';
+    window.removeEventListener('keypress', whatKey);
+    $div.append($playAgain);
   }
 }
