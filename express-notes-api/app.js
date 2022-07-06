72156c3101d83aad620701d64badc153a5a74fe2
Collin <collinhovey@gmail.com>
2022-07-06 15:07:55 +0000

    Everything but delete

---
+++ express-notes-api/app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const data = require('./data.json');
+app.use(express.json());
+const notIdError = {
+  error: 'id must be a positive integer'
+};
+
+app.get('/api/notes', (req, res) => {
+  // console.log('log whole list');
+  const sendData = [];
+  for (const x in data.notes) {
+    sendData.push(data.notes[x]);
+  }
+  res.status(200).json(sendData);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const checkId = req.params.id;
+  if (!(checkId > 0)) {
+    res.status(400).json(notIdError);
+  } else if (data.notes[checkId] === undefined) {
+    const noIdError = {
+      error: `cannot find note with id ${checkId}`
+    };
+    res.status(404).json(noIdError);
+  } else {
+    const sendData = {};
+    sendData[checkId] = data.notes[checkId];
+    res.status(200).json(sendData[checkId]);
+  }
+});
+
+app.post('/api/notes', (req, res) => {
+  const nextId = data.nextId;
+  const body = req.body;
+  if (body.content === undefined || body.content === '') {
+    const noContent = {
+      error: 'content is a required field'
+    };
+    res.status(400).json(noContent);
+  } else {
+    data.notes[nextId] = {
+      id: nextId,
+      content: body.content
+    };
+    data.nextId += 1;
+    const jsonData = JSON.stringify(data, null, 2);
+    fs.writeFile('data.json', jsonData, err => {
+      if (err) {
+        const noContent = {
+          error: 'An unexpected error occured.'
+        };
+        console.error(err);
+        res.status(500).json(noContent);
+      } else {
+        res.status(201).json(data.notes[nextId]);
+      }
+    });
+
+  }
+});
+
+app.listen(3000, () => {
+  // console.log(data);
+  // console.log('Express server is listening on port 3000');
+});
