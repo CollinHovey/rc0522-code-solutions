f9d9ea834187b58a61e2de5407aef9a8b823a37c
Collin Hovey <collinhovey@gmail.com>
2022-07-13 19:41:02 +0000

    Completed ES6 Modules

--- es6-modules/src/components/todo-list.js
+++ es6-modules/src/components/todo-list.js
@@ -1,4 +1,5 @@
-const { createElement } = require('../lib');
+// const { createElement } = require('../lib');
+import { createElement } from '../lib';
 
 class TodoList {
   constructor(onToggle) {
@@ -49,4 +50,4 @@ class TodoList {
   }
 }
 
-module.exports = TodoList;
+export default TodoList;
