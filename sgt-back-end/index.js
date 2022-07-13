401a71a07243275cfcf3c253b6940cb40deadacf
Collin Hovey <collinhovey@gmail.com>
2022-07-13 10:18:10 +0000

    Stuck on first one

---
+++ sgt-back-end/index.js
@@ -0,0 +1,33 @@
+const pg = require('pg');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/api/grades', (req, res, next) => {
+  const sql = `
+    select *
+      from "grades"
+  `;
+  db.query(sql)
+    .then(result => {
+      const grades = result;
+      res.json(grades);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
+
+// app.listen(5432, () => {
+//   console.log('Express server is listening on port 3000');
+// });
