0a4ce85943a5959abd97942bdda8c425bf544a7a
Collin <collinhovey@gmail.com>
2022-07-15 01:24:34 +0000

    Started

---
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -0,0 +1,19 @@
+// import React from 'react';
+
+// class ToggleSwitch extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick() {
+
+//   }
+
+//   render() {
+
+//   }
+// }
+
+// export default ToggleSwitch;
