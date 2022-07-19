50f4e0701f312b51203012bd9fb6e49390b84721
Collin <collinhovey@gmail.com>
2022-07-19 02:55:51 +0000

    Made Fixes

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      openId: null
+    };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(id) {
-    if (this.state[id] === undefined) {
-      this.setState({ [id]: true });
+    if (this.state.openId === id) {
+      this.setState({ openId: null });
     } else {
-      this.setState({ [id]: !this.state[id] });
+      this.setState({ openId: id });
     }
 
   }
@@ -21,9 +23,9 @@ class Accordion extends React.Component {
     const listInput = input.map((entry, index) => {
       entry.id = index;
       return (
-      <div key={entry.header} className='entry-container'>
+      <div key={entry.id} className='entry-container'>
         <div className="entry-header" onClick={() => this.handleClick(entry.id)}>{entry.header}</div>
-          <div className={`entry-description clicked-${this.state[entry.id]}`}>{entry.description}</div>
+          <div className={`entry-description clicked-${this.state.openId === entry.id ? 'true' : 'false'}`}>{entry.description}</div>
       </div>
       );
     });
